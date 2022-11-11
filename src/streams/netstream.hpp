9ea66697fcc5fb2c06f9d9bfe553c19b152974aa
Nicolas Pope <nicolas.pope@utu.fi>
2022-11-11 07:46:27 +0000

    Cleanup and stats for netstream buffering

--- src/streams/netstream.hpp
+++ src/streams/netstream.hpp
@@ -103,6 +103,8 @@ class Net : public Stream {
     ftl::PacketManager mgr_;
     ftl::Handler<ftl::net::Peer*> connect_cb_;
     int64_t buffering_ = 0;
+    std::atomic_int underuns_ = 0;
+    std::atomic_int drops_ = 0;
 
     static std::atomic_size_t req_bitrate__;
     static std::atomic_size_t tx_bitrate__;
