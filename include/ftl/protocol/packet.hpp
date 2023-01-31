9c82bd2271993a4c8b85e66aea3e5139220a9877
Nicolas Pope <nicolas.pope@utu.fi>
2023-01-31 08:32:26 +0000

    Add ephemeral flag

--- include/ftl/protocol/packet.hpp
+++ include/ftl/protocol/packet.hpp
@@ -22,6 +22,7 @@ static constexpr uint8_t kFlagCompleted = 0x02;  ///< Last packet for timestamp
 static constexpr uint8_t kFlagReset = 0x04;      ///< Request full data, including key frames.
 static constexpr uint8_t kFlagFull = 0x04;       ///< If set on EndFrame packet then that frame contained full data
 static constexpr uint8_t kFlagOutOfBand = 0x08;  ///< The data is not tied to a specific frame rate
+static constexpr uint8_t kFlagEphemeralChannel = 0x10;    ///< Don't keep this data in a session file
 
 static constexpr uint8_t kAllFrames = 255;
 static constexpr uint8_t kAllFramesets = 255;
